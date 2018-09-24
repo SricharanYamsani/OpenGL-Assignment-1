94ff66565b8f1814c142d48b9119f3da540ba77b
SricharanYamsani <43514338+SricharanYamsani@users.noreply.github.com>
2018-09-24 23:13:08 +0530

    Updated Shapes.h
    Added Circle class

--- Shapes.h
+++ Shapes.h
@@ -1,7 +1,7 @@
 #pragma once
 #include<GL/glew.h>
 #include<vector>
-#include<glm.hpp>
+#include<glm/glm.hpp>
 
 class Shape
 {
@@ -11,6 +11,7 @@ protected:
 public:
 	Shape()
 	{
+		std::cout << "In shape COnstuctor\n";
 		glGenVertexArrays(1, &VAO);
 		glGenBuffers(1, &VBO);
 	}
@@ -19,6 +20,8 @@ public:
 		glDeleteVertexArrays(1, &VAO);
 		glDeleteBuffers(1, &VBO);
 	}
+
+	virtual void draw() = 0;
 };
 
 class Circle : public Shape
@@ -37,19 +40,28 @@ public:
 
 	}
 
-	void SetPositions(glm::vec2 centre, glm::vec2 circumfernce_point)
+	void SetOriginPosition(glm::vec2 centre)
 	{
 		positions[0] = centre;
-		positions[1] = circumfernce_point;
+		//positions[1] = circumfernce_point;
 	}
+
+	void SetCirclePosition(glm::vec2 circumfernce_point)
+	{
+	//positions[0] = centre;
+	positions[1] = circumfernce_point;
+	}
+
 	void SetAngleDiff(float diff)
 	{
 		angleDiff = diff;
 	}
+
 	void SetBufferData()
 	{
 		float radius = glm::distance(positions[0], positions[1]);
-		glm::vec2 point = glm::vec2(0.0f,0.0f);
+		glm::vec2 point = glm::vec2(0.0f, 0.0f);
+		vertices.push_back(point);
 		for (float i = 0.0f; i < 360.0f; i += angleDiff)
 		{
 			point.x = cos(glm::radians(i))*radius;
@@ -58,5 +70,22 @@ public:
 			vertices.push_back(point);
 		}
 		vertices.push_back(glm::vec2(radius, 0.0f));
+	}
+
+	void draw()
+	{
+		SetBufferData();
+
+		glBindVertexArray(VAO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec2), vertices.data(), GL_STATIC_DRAW);
+
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+
+		glDrawArrays(GL_TRIANGLE_FAN, 0, vertices.size());
+
+		glBindVertexArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
 	}
 };
