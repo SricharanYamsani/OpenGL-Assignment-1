a51f1bc28fca3412cc7d84aee9928e7df4d13db3
SricharanYamsani <43514338+SricharanYamsani@users.noreply.github.com>
2018-09-25 12:19:50 +0530

    Update Shapes.h
    Added Rectangle and Line Classes

--- Shapes.h
+++ Shapes.h
@@ -1,7 +1,9 @@
 #pragma once
 #include<GL/glew.h>
 #include<vector>
-#include<glm/glm.hpp>
+#include<glm.hpp>
+
+const int SCR_WIDTH = 800, SCR_HEIGHT = 600;
 
 class Shape
 {
@@ -22,6 +24,9 @@ public:
 	}
 
 	virtual void draw() = 0;
+	virtual void SetOriginPosition(glm::vec2 origin) = 0;
+	virtual void SetOtherPosition(glm::vec2 _point) = 0;
+	virtual void SetBufferData() = 0;
 };
 
 class Circle : public Shape
@@ -33,23 +38,23 @@ public:
 	Circle()
 	{
 		positions[0] = positions[1] = glm::vec2(0.0f, 0.0f);
-		angleDiff = 30.0f;
+		angleDiff = 10.0f;
 	}
 	~Circle()
 	{
 
 	}
 
-	void SetOriginPosition(glm::vec2 centre)
+	void SetOriginPosition(glm::vec2 origin)
 	{
-		positions[0] = centre;
+		positions[0] = origin;
 		//positions[1] = circumfernce_point;
 	}
 
-	void SetCirclePosition(glm::vec2 circumfernce_point)
+	void SetOtherPosition(glm::vec2 _point)
 	{
-	//positions[0] = centre;
-	positions[1] = circumfernce_point;
+		//positions[0] = centre;
+		positions[1] = _point;
 	}
 
 	void SetAngleDiff(float diff)
@@ -62,11 +67,11 @@ public:
 		vertices.clear();
 		float radius = glm::distance(positions[0], positions[1]);
 		glm::vec2 point = positions[0];
-		vertices.push_back(point);
+		//vertices.push_back(point);
 		for (float i = 0.0f; i < 360.0f; i += angleDiff)
 		{
 			point.x = positions[0].x + cos(glm::radians(i))*radius;
-			point.y = positions[0].y + sin(glm::radians(i))*radius;
+			point.y = positions[0].y + sin(glm::radians(i))*radius * SCR_WIDTH/(float)SCR_HEIGHT;
 
 			vertices.push_back(point);
 		}
@@ -86,7 +91,7 @@ public:
 			glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
 			glEnableVertexAttribArray(0);
 
-			glDrawArrays(GL_TRIANGLE_FAN, 0, vertices.size());
+			glDrawArrays(GL_LINE_LOOP, 0, vertices.size());
 
 			glBindVertexArray(0);
 			glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -96,4 +101,117 @@ public:
 			//std::cout << "Center is Same as Circle Point";
 		}
 	}
+};
+
+class Line : public Shape
+{
+	glm::vec2 positions[2];
+	std::vector<glm::vec2> vertices;
+
+public:
+	Line()
+	{
+		positions[0] = positions[1] = glm::vec2(0.0f, 0.0f);
+	}
+	~Line()
+	{
+
+	}
+	void SetOriginPosition(glm::vec2 origin)
+	{
+		positions[0] = origin;
+	}
+	void SetOtherPosition(glm::vec2 _point)
+	{
+		positions[1] = _point;
+	}
+	void SetBufferData()
+	{
+		vertices.clear();
+		vertices.push_back(positions[0]);
+		vertices.push_back(positions[1]);
+	}
+	void draw()
+	{
+		if (positions[0] != positions[1])
+		{
+			SetBufferData();
+			glBindVertexArray(VAO);
+			glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+			glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec2), vertices.data(), GL_STATIC_DRAW);
+
+			glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+			glEnableVertexAttribArray(0);
+
+			glDrawArrays(GL_LINES, 0, vertices.size());
+
+			glBindVertexArray(0);
+			glBindBuffer(GL_ARRAY_BUFFER, 0);
+		}
+	}
+};
+
+class Rectangle : public Shape
+{
+	glm::vec2 positions[2];
+	std::vector<glm::vec2> vertices;
+
+public:
+	Rectangle()
+	{
+		positions[0] = positions[1] = glm::vec2(0.0f, 0.0f);
+	}
+	~Rectangle()
+	{
+
+	}
+	void SetOriginPosition(glm::vec2 origin)
+	{
+		positions[0] = origin;
+	}
+	void SetOtherPosition(glm::vec2 _point)
+	{
+		positions[1] = _point;
+	}
+	void SetBufferData()
+	{
+		vertices.clear();
+		float xdiff = positions[1].x - positions[0].x;
+		float ydiff = positions[1].y - positions[0].y;
+
+		glm::vec2 point = glm::vec2(0.0f,0.0f);
+
+		point = positions[0];
+		vertices.push_back(point);
+
+		point = glm::vec2(positions[0].x, positions[1].y);
+		vertices.push_back(point);
+
+		point = positions[1];
+		vertices.push_back(point);
+
+		point = glm::vec2(positions[1].x, positions[0].y);
+		vertices.push_back(point);
+	}
+	void draw()
+	{
+		if (positions[0] != positions[1])
+		{
+			SetBufferData();
+			glBindVertexArray(VAO);
+			glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+			glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec2), vertices.data(), GL_STATIC_DRAW);
+
+			glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+			glEnableVertexAttribArray(0);
+
+			glDrawArrays(GL_LINE_LOOP, 0, vertices.size());
+
+			glBindVertexArray(0);
+			glBindBuffer(GL_ARRAY_BUFFER, 0);
+		}
+	}
+
 };
